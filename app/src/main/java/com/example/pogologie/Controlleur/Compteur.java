8c3fd1896767b51d7c58bc8240e3de079690b9e0
Gogoth <hugo.chauviere2000@gmail.com>
2021-04-14 14:07:11 +0200

    compteur pompes et affichage

--- app/src/main/java/com/example/pogologie/Controlleur/Compteur.java
+++ app/src/main/java/com/example/pogologie/Controlleur/Compteur.java
@@ -6,11 +6,21 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import com.example.pogologie.R;
 
+import java.util.Calendar;
+
 public class Compteur extends AppCompatActivity {
     private Button bt1;
+    private TextView compteurPompes;
+    private TextView compteurPompesTotal;
+    private int semaineActuelle;
+    private int anneeActuelle;
+    private String nombrePompes;
+    private int compteurTotal=200;
+    private Calendar calendrier;
 
 
     @Override
@@ -18,6 +28,8 @@ public class Compteur extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_compteur);
         bt1 = (Button) findViewById(R.id.compteur_bt1);
+        compteurPompes=(TextView)findViewById(R.id.compteurNbPompes);
+        compteurPompesTotal=(TextView)findViewById(R.id.compteurNbPompesTotal);
 
         bt1.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -26,5 +38,23 @@ public class Compteur extends AppCompatActivity {
                 startActivity(gameActivity);
             }
         });
+        nombrePompes="";
+        calendrier=Calendar.getInstance();
+        semaineActuelle = calendrier.get(Calendar.WEEK_OF_YEAR);
+        anneeActuelle = calendrier.get(Calendar.YEAR);
+        if(anneeActuelle>2021){
+            semaineActuelle+=(anneeActuelle-2021)*52;
+        }
+        semaineActuelle+=16;
+        nombrePompes+=semaineActuelle;
+        compteurPompes.setText(nombrePompes);
+        for(int i=20;i<semaineActuelle;i++){
+            compteurTotal+=6*i;
+        }
+        compteurTotal+=31*(calendrier.get(Calendar.DAY_OF_WEEK)-1);
+        nombrePompes=""+compteurTotal;
+        compteurPompesTotal.setText(nombrePompes);
     }
+
+
 }
--- app/src/main/java/com/example/pogologie/Controlleur/MainActivity.java
+++ app/src/main/java/com/example/pogologie/Controlleur/MainActivity.java
@@ -14,42 +14,24 @@ import android.widget.TextView;
 import com.example.pogologie.R;
 
 public class MainActivity extends AppCompatActivity {
-    private TextView text_main;
     private Button button_main;
-    private EditText edit_main;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        edit_main = (EditText) findViewById(R.id.activity_main_et1);
         button_main = (Button) findViewById(R.id.activity_main_btn1);
-        text_main = (TextView)findViewById(R.id.activity_main_tv1);
 
-        button_main.setEnabled(false);
+        button_main.setEnabled(true);
 
-        edit_main.addTextChangedListener(new TextWatcher() {
-            @Override
-            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-
-            }
-
-            @Override
-            public void onTextChanged(CharSequence s, int start, int before, int count) {
-                button_main.setEnabled(s.toString().length() != 0);
-            }
-
-            @Override
-            public void afterTextChanged(Editable s) {
-
-            }
-        });
         button_main.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                Intent gameActivity = new Intent(MainActivity.this, Compteur.class);
-                startActivity(gameActivity);
-            }
+            public void onClick(View v) {openCompteurActivity();}
         });
 
+    }
+
+    private void openCompteurActivity() {
+        Intent intentCompteur = new Intent(this,Compteur.class);
+        startActivity(intentCompteur);
     }
 }
